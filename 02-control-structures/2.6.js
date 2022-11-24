94a57250a52bc0216a837a128553802bc9ddd87a
Virginie Dourson <virginiedourson@gmail.com>
2022-11-24 15:10:37 +0100

    Exercice 2.6

---
+++ 02-control-structures/2.6.js
@@ -0,0 +1,27 @@
+let day=Number(window.prompt("Give a number from 1 to 7","Number"));
+    switch (day){
+        case 1:
+            console.log("Monday");
+            break;
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+        case 7:
+            console.log("Sunday");
+            break;
+        default:
+            console.log("Le nombre n'est pas compris entre 1 et 7");
+            break;
+    }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # algo-js
 Algorithmics with JavaScript
+
+This repository is meant to do exercices to learn Javascript from the beginning with Becode.
