727c4a7a02f56bb331dc8001993a5435cb02bed4
Virginie Dourson <virginiedourson@gmail.com>
2022-11-24 14:21:39 +0100

    Exercice 2.2 corrigé

--- 2.2.js
+++ 2.2.js
@@ -1,14 +1,14 @@
-let petit = prompt("Entrer un nombre","Nombre");
-let grand = prompt("Entrer un nombre supérieur au nombre précédent","Nombre supérieur");
-if (grand<=petit){
+let petit = Number(prompt("Entrer un nombre","Nombre"));
+let grand = Number(prompt("Entrer un nombre supérieur au nombre précédent","Nombre supérieur"));
+if (grand <= petit){
     console.log("Le deuxième nombre que vous avez entré est plus petit ou égal au premier, ce programme ne peut donc pas s'exécuter correctement");
 }
 else {
-    let courant = prompt("Entrer un troisième nombre au hasard","Nombre au hasard");
-    if ((courant>petit)||(courant<grand)) {
-        console.log("Votre troisième nombre est compris entre les deux premiers");
-    }
-    else{
-        console.log("Votre troisième nombre n'est pas compris entre les deux premiers");
-    }
+    let courant = Number(prompt("Entrer un troisième nombre au hasard","Nombre au hasard"));
+if ((courant>petit)&&(courant<grand)) {
+    console.log("Votre troisième nombre est compris entre les deux premiers");
+}
+else{
+    console.log("Votre troisième nombre n'est pas compris entre les deux premiers");
+}
 }
