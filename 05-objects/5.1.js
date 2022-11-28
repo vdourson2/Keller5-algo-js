e43af9130c76f188b7feb42d0dc76595426e920f
Virginie Dourson <virginiedourson@gmail.com>
2022-11-28 10:56:39 +0100

    Exercice 5.1

---
+++ 05-objects/5.1.js
@@ -0,0 +1,15 @@
+//This function
+function askTvSerie(){
+    let favorite = { };
+    favorite.name=prompt("What is the name of your favorite TV serie","Name");
+    favorite.year=prompt("What is the year of production of this TV serie","Year");
+    favorite.cast=[];
+    let n=Number(prompt("How many cast members do you remember from this TV serie?","Number"));
+    for (let i=1;i<=n;i++){
+        favorite.cast.push(prompt("Give the name of cast member number" + i ,"Name"))
+    }
+    return favorite
+}
+console.log(JSON.stringify(askTvSerie()));
+
+
